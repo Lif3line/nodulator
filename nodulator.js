4c86db42ec95df30826be18f1a1906a5c40e5db9
Adam Hartwell <adamhartwell2@gmail.com>
2014-02-28 17:52:14 +0000

    Added node generation from uploaded images
    Uploaded images will now be processed to generate an array of colourful
    nodes ready for manipulation. Added a default picture to use.

--- nodulator.js
+++ nodulator.js
@@ -8,16 +8,76 @@
 var Nodulator = {
 
 	settings: {
-		inputID: 'imageLoader' //ID of image input element to add listener to
+		inputID: 'imageLoader', //ID of image input element to add listener to
+		canvasID: 'nodeBox',
+
+		nodeSparsity: 12, //Distance in px between nodes on creation
+		drawThreshold: 220 //Grayscale pixel threshold; pixels whiter than this value will not be converted to nodes (0-255)
 	},
 
+	canvas: null, //Canvas data
+	ctx: null,       //Canvas context data
+
+	nodes: [], // Array for nodes; each node is an object containing current and initial X & Y co-ordinates and the nodes colour
+
+	/*** Add listeners then start animating using a default image ***/
 	init: function() {
+		this.canvas = document.getElementById(this.settings.canvasID);
+		this.ctx = this.canvas.getContext('2d');
+
 		document.getElementById(this.settings.inputID).addEventListener('change', this.updateImage, false);
+	},
+
+	/*** Note: 'this' context change***/
+	updateImage: function(e) {
+		var reader = new FileReader();
+		reader.onload = function(event){
+			Nodulator.generateNodes(event.target.result);
+		}
+	},
+
+	/*** Take in an image source and convert it into the titular nodes***/
+	generateNodes: function(src) {
+		var img = new Image();
+
+		img.onload = function() { //Draw onto canvas to access pixel data
+			Nodulator.ctx.clearRect(0, 0, Nodulator.canvas.width, Nodulator.canvas.height); //Clear canvas
+
+			var offsetX = Math.floor((Nodulator.canvas.width - img.width) / 2); //Center image within canvas
+			var offsetY = Math.floor((Nodulator.canvas.height - img.height) / 2);
+
+			Nodulator.ctx.drawImage(img, offsetX, offsetY); //Actual drawing
+
+			/* Get image data and associated pixel data to work with */
+			var imageData = Nodulator.ctx.getImageData(0, 0, Nodulator.canvas.width, Nodulator.canvas.height);
+			var pixelData = imageData.data;
+			var i, j, pos, colour, node; //Variables for loop
+
+			/* Loop to check each pixel in a grid with spaces defined by this.nodeSparsity*/
+			for (i = 0; i < Nodulator.canvas.width; i += Nodulator.settings.nodeSparsity) {
+				for (j = 0; j < Nodulator.canvas.height; j += Nodulator.settings.nodeSparsity) {
+					pos = (i + (j * imageData.width)) * 4;
+					/* Data set up in 1D array: R-G-B-A and repeat */
+					if (pixelData[pos] > Nodulator.settings.drawThreshold && pixelData[pos + 1] > Nodulator.settings.drawThreshold
+					&& pixelData[pos + 2] > Nodulator.settings.drawThreshold  || pixelData[pos + 3] === 0) {
+						continue; // Ignore pixels that are too white or are transparent (canvas background)
+					} else {
+
+						colour = 'rgba(' + pixelData[pos] + ',' + pixelData[pos + 1] + ',' + pixelData[pos + 2] + ',' + '1)';
+
+						node = { //Node object literal
+							x: i,
+							y: j,
+							initX: i,
+							initY: j,
+							colour: colour
+						}
+						Nodulator.nodes.push(node); //Append to the array of nodes
+					}
+				}
+			}
+		};
+		img.src = src;
 	}
-
-	updateImage: function() {
-
-	}
-
 
 }
